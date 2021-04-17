19cd15e59dca181295b032597f45fea893f7412a
emetdas <emet@b2bleadstaff.com>
2021-04-17 13:38:00 +0600

    code update

--- array/array-iteration.js
+++ array/array-iteration.js
@@ -1,6 +1,9 @@
 var txt = "";
 var numbers = [45, 4, 9, 16, 25];
-numbers.forEach(myFunction);
-function myFunction(value, index, array) {
-  console.log(txt = txt + value);
+// numbers.forEach(myFunction);
+// function myFunction(value, index, array) {
+//   console.log(txt = txt + value);
+// }
+for(var i = 0; i<numbers.length;i++){
+console.log(numbers[i]);
 }
