077a23cc118e8c61d3b68b3cf0758ea2f2ae6e96
emetdas <emet@b2bleadstaff.com>
2021-04-18 21:48:33 +0600

    today code is done

---
+++ number/main.js
@@ -0,0 +1,17 @@
+// string to Number
+var a = "80";
+console.log(Number(a));
+// string to Number
+var num = "10.90";
+console.log(parseInt(num));
+console.log(parseFloat(num));
+var num = 10.909;
+console.log(num.toFixed(2));
+var num = 10.409;
+console.log(num.toPrecision(4));
+
+
+
+
+
+
