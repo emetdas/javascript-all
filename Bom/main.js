e8cab53fdd7ea024acf12518ad02b4997dd255d4
emetdas <emet@b2bleadstaff.com>
2021-04-19 13:19:34 +0600

    all update

---
+++ Bom/main.js
@@ -0,0 +1,13 @@
+var open;
+function opens(){
+   open =  window.open("https://b2bleadstaff.com/","_blank","width=400px,height=400px,left=200px,top=200px");
+}
+function closes(){
+    open.close();
+}
+
+
+
+
+
+
