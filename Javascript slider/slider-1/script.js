671ca854dc0980f321426c6c3a7237400f2280a6
emetdas <emet@b2bleadstaff.com>
2021-08-01 16:35:32 +0600

    slider one is done but destactureing is not done.

--- Javascript slider/slider-1/script.js
+++ Javascript slider/slider-1/script.js
@@ -6,15 +6,54 @@ let prev = document.querySelector('.left');
 var index = 0;
 var Lenght = slide.length;
 var persent = Math.round(100 / Lenght);
-// slide.forEach((e) => {
-//   console.log(e.getBoundingClientRect().width);
-// });
+
 slider.style.width = `${Lenght * 100}%`;
+let dots = document.querySelector('.dots');
+let creatDots = () => {
+  slide.forEach((_, i) => {
+    dots.insertAdjacentHTML(
+      'beforeend',
+      `<li class="controler-dot" data-dot="${i}">
+        </li>`
+    );
+  });
+};
+creatDots();
+function activeDotOne() {
+  let activeDot = dots.children[0];
+  activeDot.classList.add('active');
+}
+activeDotOne();
+
+function setIndex(section) {
+  slider.style.transform = 'translate(' + section * -persent + '%)';
+}
+function removeSlide() {
+  document.querySelectorAll('.dots .controler-dot').forEach(function (i) {
+    i.classList.remove('active');
+  });
+}
+
+document
+  .querySelectorAll('.dots .controler-dot')
+  .forEach(function (indecator, i) {
+    indecator.addEventListener('click', function () {
+      index = i;
+      removeSlide();
+      indecator.classList.add('active');
+      setIndex(index);
+    });
+  });
+
 next.addEventListener('click', function () {
   index = index < Lenght - 1 ? index + 1 : Lenght - 1;
-  slider.style.transform = 'translate(' + index * -persent + '%)';
+  removeSlide();
+  dots.children[index].classList.add('active');
+  setIndex(index);
 });
 prev.addEventListener('click', function () {
   index = index > 0 ? index - 1 : 0;
-  slider.style.transform = 'translate(' + index * -persent + '%)';
+  removeSlide();
+  dots.children[index].classList.add('active');
+  setIndex(index);
 });
