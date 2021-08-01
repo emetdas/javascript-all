a2b53a0ec3ac302827f570cb5b11012c5ea97f59
emetdas <emet@b2bleadstaff.com>
2021-08-01 16:47:40 +0600

    all update is done

--- Javascript slider/slider-1/script.js
+++ Javascript slider/slider-1/script.js
@@ -2,13 +2,12 @@ let slider = document.querySelector('.slider');
 let slide = document.querySelectorAll('.slide');
 let next = document.querySelector('.right');
 let prev = document.querySelector('.left');
-
 var index = 0;
 var Lenght = slide.length;
 var persent = Math.round(100 / Lenght);
-
 slider.style.width = `${Lenght * 100}%`;
 let dots = document.querySelector('.dots');
+
 let creatDots = () => {
   slide.forEach((_, i) => {
     dots.insertAdjacentHTML(
@@ -19,6 +18,7 @@ let creatDots = () => {
   });
 };
 creatDots();
+
 function activeDotOne() {
   let activeDot = dots.children[0];
   activeDot.classList.add('active');
@@ -26,34 +26,26 @@ function activeDotOne() {
 activeDotOne();
 
 function setIndex(section) {
-  slider.style.transform = 'translate(' + section * -persent + '%)';
-}
-function removeSlide() {
   document.querySelectorAll('.dots .controler-dot').forEach(function (i) {
     i.classList.remove('active');
   });
+  slider.style.transform = 'translate(' + section * -persent + '%)';
 }
-
-document
-  .querySelectorAll('.dots .controler-dot')
-  .forEach(function (indecator, i) {
-    indecator.addEventListener('click', function () {
-      index = i;
-      removeSlide();
-      indecator.classList.add('active');
-      setIndex(index);
-    });
+document.querySelectorAll('.dots .controler-dot').forEach((indecator, i) => {
+  indecator.addEventListener('click', () => {
+    index = i;
+    setIndex(index);
+    indecator.classList.add('active');
   });
-
-next.addEventListener('click', function () {
-  index = index < Lenght - 1 ? index + 1 : Lenght - 1;
-  removeSlide();
-  dots.children[index].classList.add('active');
-  setIndex(index);
 });
-prev.addEventListener('click', function () {
-  index = index > 0 ? index - 1 : 0;
-  removeSlide();
-  dots.children[index].classList.add('active');
+
+next.addEventListener('click', () => {
+  index = index < Lenght - 1 ? index + 1 : Lenght - 1;
   setIndex(index);
+  dots.children[index].classList.add('active');
+});
+prev.addEventListener('click', () => {
+  index = index > 0 ? index - 1 : 0;
+  setIndex(index);
+  dots.children[index].classList.add('active');
 });
