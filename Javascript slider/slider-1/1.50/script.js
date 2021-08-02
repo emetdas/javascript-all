7f603626692332cbd056528100278f7ce1ac48c4
emetdas <emet@b2bleadstaff.com>
2021-08-03 00:18:59 +0600

    js

--- Javascript slider/slider-1/1.50/script.js
+++ Javascript slider/slider-1/1.50/script.js
@@ -8,6 +8,7 @@ var persent = Math.round(100 / Lenght);
 slider.style.width = `${Lenght * 100}%`;
 let dots = document.querySelector('.dots');
 
+
 let creatDots = () => {
   slide.forEach((_, i) => {
     dots.insertAdjacentHTML(
