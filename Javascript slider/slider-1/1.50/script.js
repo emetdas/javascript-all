6f1357f0ac94624693b0176d3d93d709577f5fda
emetdas <emet@b2bleadstaff.com>
2021-08-06 13:10:34 +0600

    some update is done

--- Javascript slider/slider-1/1.50/script.js
+++ Javascript slider/slider-1/1.50/script.js
@@ -2,15 +2,7 @@ let slider = document.querySelector('.slider');
 let slide = document.querySelectorAll('.slide');
 let next = document.querySelector('.right');
 let prev = document.querySelector('.left');
-var index = 0;
-var Lenght = slide.length;
-var persent = Math.round(100 / Lenght);
-slider.style.width = `${Lenght * 100}%`;
 let dots = document.querySelector('.dots');
-let sliderWidth = slide[0].getBoundingClientRect().width;
-
-
-
 let creatDots = () => {
   slide.forEach((_, i) => {
     dots.insertAdjacentHTML(
@@ -21,8 +13,14 @@ let creatDots = () => {
   });
 };
 creatDots();
-
+var index = 0;
+var Lenght = slide.length;
+slider.style.width = `${Lenght * 100}%`;
+let sliderWidth = slide[0].getBoundingClientRect().width;
+const dot = document.querySelectorAll('.controler-dot');
 // create slider append and prepend slide
+// slider.insertAdjacentHTML('afterbegin', slide[Lenght - 1].outerHTML);
+// slider.insertAdjacentHTML('beforeend', slide[0].outerHTML);
 
 function activeDotOne() {
   let activeDot = dots.children[0];
@@ -30,27 +28,78 @@ function activeDotOne() {
 }
 activeDotOne();
 
-function setIndex(section) {
-  document.querySelectorAll('.dots .controler-dot').forEach(function (i) {
-    i.classList.remove('active');
+function removedot() {
+  dot.forEach((ac) => {
+    ac.classList.remove('active');
   });
-  slider.style.transform = 'translate(' + section * -persent + '%)';
 }
-document.querySelectorAll('.dots .controler-dot').forEach((indecator, i) => {
-  indecator.addEventListener('click', () => {
+function activeDot() {
+  dot[index].classList.add('active');
+}
+
+next.addEventListener('click', () => {
+  slider.style.transition = 'all 0.5s ease-in-out';
+  index++;
+  slider.style.transform = `translateX(${-sliderWidth * index}px)`;
+  removedot();
+  if (index > Lenght - 1) {
+    setTimeout(() => {
+      index = 0;
+      slider.style.transform = `translateX(0px)`;
+      activeDot();
+      slider.style.transition = 'none';
+    }, 300);
+  } else {
+    activeDot();
+  }
+});
+prev.addEventListener('click', () => {
+  slider.style.transition = 'all 0.5s ease-in-out';
+  index--;
+  slider.style.transform = `translateX(${-sliderWidth * index}px)`;
+  removedot();
+  if (index < 0) {
+    setTimeout(() => {
+      index = Lenght - 1;
+      slider.style.transform = `translateX(${-sliderWidth * index}px)`;
+      activeDot();
+      slider.style.transition = 'none';
+    }, 300);
+  } else {
+    activeDot();
+  }
+});
+
+dot.forEach((dotone, i) => {
+  dotone.addEventListener('click', () => {
+    dot.forEach((dotremove) => dotremove.classList.remove('active'));
     index = i;
-    setIndex(index);
-    indecator.classList.add('active');
+    dotone.classList.add('active');
+    slider.style.transform = `translateX(${-sliderWidth * index}px)`;
   });
 });
 
-next.addEventListener('click', () => {
-  index = index < Lenght - 1 ? index + 1 : Lenght - 1;
-  setIndex(index);
-  dots.children[index].classList.add('active');
-});
-prev.addEventListener('click', () => {
-  index = index > 0 ? index - 1 : 0;
-  setIndex(index);
-  dots.children[index].classList.add('active');
-});
+// function setIndex(section) {
+//   document.querySelectorAll('.dots .controler-dot').forEach(function (i) {
+//     i.classList.remove('active');
+//   });
+//   slider.style.transform = 'translate(' + section * -persent + '%)';
+// }
+// document.querySelectorAll('.dots .controler-dot').forEach((indecator, i) => {
+//   indecator.addEventListener('click', () => {
+//     index = i;
+//     setIndex(index);
+//     indecator.classList.add('active');
+//   });
+// });
+
+// next.addEventListener('click', () => {
+//   index = index < Lenght - 1 ? index + 1 : Lenght - 1;
+//   setIndex(index);
+//   dots.children[index].classList.add('active');
+// });
+// prev.addEventListener('click', () => {
+//   index = index > 0 ? index - 1 : 0;
+//   setIndex(index);
+//   dots.children[index].classList.add('active');
+// });
