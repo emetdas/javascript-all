381bdc2a35344f7cd8256879c6aa802c9f1bb084
emetdas <emet@b2bleadstaff.com>
2021-08-08 10:28:52 +0600

    new update is done

---
+++ Javascript slider/slider-1/1.80/commingSoon.js
@@ -0,0 +1,10 @@
+setInterval(() => {
+  let currentDate = new Date().getTime();
+  let launceDate = new Date('Aug 21, 2021 10:00:00').getTime();
+  let duration = currentDate - launceDate;
+  var day = Math.floor(duration / (1000 * 60 * 60 * 24));
+  let hour = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minute = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+  let second = Math.floor((duration % (1000 * 60)) / 1000);
+  console.log(`day: ${day} Hour: ${hour} Minute: ${minute} Second: ${second}`);
+}, 1000);
