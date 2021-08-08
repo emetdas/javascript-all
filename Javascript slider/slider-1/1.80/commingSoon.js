3f99e652bb866f0b6e68902b1c0034f16253992f
emetdas <emet@b2bleadstaff.com>
2021-08-08 10:40:26 +0600

    new comming soon time code is update is here

--- Javascript slider/slider-1/1.80/commingSoon.js
+++ Javascript slider/slider-1/1.80/commingSoon.js
@@ -1,7 +1,7 @@
 setInterval(() => {
   let currentDate = new Date().getTime();
   let launceDate = new Date('Aug 21, 2021 10:00:00').getTime();
-  let duration = currentDate - launceDate;
+  let duration = launceDate - currentDate;
   var day = Math.floor(duration / (1000 * 60 * 60 * 24));
   let hour = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minute = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
