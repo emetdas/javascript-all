5d3d4a7ccd3aae42466d3602648f6870cae8daa5
emetdas <emet@b2bleadstaff.com>
2021-08-12 14:05:37 +0600

    update

--- Javascript slider/slider-1/1.80/script.js
+++ Javascript slider/slider-1/1.80/script.js
@@ -2,4 +2,5 @@ let slider = document.querySelector(".slider");
 let slide = document.querySelectorAll(".slide");
 
 let controler = document.querySelector(".controler");
+let slide = document.querySelector(".slide");
 let dots = document.querySelectorAll(".dot");
