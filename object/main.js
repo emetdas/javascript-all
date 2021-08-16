9d9333a62db9c03c2b7b63d392a032526d0d7509
emetdas <emet@b2bleadstaff.com>
2021-08-16 20:26:00 +0600

    new update is done

--- object/main.js
+++ object/main.js
@@ -1,13 +1,17 @@
 var obj = {
-    name:"Emet",
-    Develoepr:"opp",
-    age:20,
-    pro:{
-        1:"PHP",
-        1:"Java"
-    }
+  name: 'Emet',
+  Develoepr: 'opp',
+  age: 20,
+  pro: {
+    1: 'PHP',
+    1: 'Java',
+  },
+};
+// console.log(obj.pro);
+// result = JSON.stringify(obj);
+// console.log(result);
+// console.log(JSON.parse(result));
+for (ittert in obj) {
+  console.log(ittert);
+  
 }
-console.log(obj.pro);
-result = JSON.stringify(obj);
-console.log(result);
-console.log(JSON.parse(result));
