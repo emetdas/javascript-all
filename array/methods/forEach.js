7d249d2d8f068ed4b46c360083fd1436ac0e4206
emetdas <emet@b2bleadstaff.com>
2021-09-14 17:04:56 +0600

    update is done

--- array/methods/forEach.js
+++ array/methods/forEach.js
@@ -1,1 +1,21 @@
 let array = [1, 2, 10, 7, 1000, 20, 10];
+let obj = {
+  name: 'Emet Das',
+  Positone: 'Web Developer',
+  skills: ['Html5', 'Css3', 'Sass', 'JavaScript'],
+};
+
+// ForEach
+// array.forEach((a, i) => {
+//   console.log(a, i);
+// });
+
+// for of loop ittrate array
+// for (const a,i of array) {
+//   console.log(a,i);
+// }
+
+// for in loop ittrate object
+for (const i in obj) {
+  console.log(i);
+}
