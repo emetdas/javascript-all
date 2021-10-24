df8314939d3ce1dc66a1506f169db3ca7d580d15
emetdas <emet@b2bleadstaff.com>
2021-10-24 10:02:29 +0600

    array sort method is done

---
+++ array/methods/sort.js
@@ -0,0 +1,10 @@
+let array = [2, 3, 5, 10, 44, 1];
+let assinding = array.sort((a, b) => {
+  return a - b;
+});
+console.log(assinding);
+let arr = [2, 3, 5, 10, 44, 1];
+let dessinding = arr.sort((a, b) => {
+  return b - a;
+});
+console.log(dessinding);
