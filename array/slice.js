556adb1488870f1da32104ab10513b43c9e5faa4
emetdas <emet@b2bleadstaff.com>
2021-10-26 00:24:06 +0600

    update

--- array/slice.js
+++ array/slice.js
@@ -1,7 +1,5 @@
-let numbers = [10,2,3];
-let numbers2 = numbers.slice(-2);
-let numbers3 = numbers.splice(0,1);
-console.log(numbers);
-console.log(numbers3);
+let numbers = [10, 2, 3];
+let numbers2 = numbers.slice(-1);
 console.log(numbers2);
-console.log(numbers);
+let numbers3 = numbers.splice(0, 1);
+console.log(numbers3);
