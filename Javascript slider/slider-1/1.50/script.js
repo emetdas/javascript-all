d451664ed261fdab2fd9898c3b4fc2efc35317b0
emetdas <emet@b2bleadstaff.com>
2022-02-13 21:22:18 +0600

    ___

--- Javascript slider/slider-1/1.50/script.js
+++ Javascript slider/slider-1/1.50/script.js
@@ -78,6 +78,7 @@ dot.forEach((dotone, i) => {
     slider.style.transform = `translateX(${-sliderWidth * index}px)`;
   });
 });
+
 // function setIndex(section) {
 //   document.querySelectorAll('.dots .controler-dot').forEach(function (i) {
 //     i.classList.remove('active');
