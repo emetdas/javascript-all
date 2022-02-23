94493a0e765cb4670d116599bcbc531dc81ddfe6
emetdas <emet@b2bleadstaff.com>
2022-02-23 20:00:06 +0600

    __update

--- typescript setup/dist/index.js
+++ typescript setup/dist/index.js
@@ -1,15 +1,15 @@
 "use strict";
 var id = 20;
-var developers = ['Emet', 'Jonas', 'Dev Ed'];
+var developers = ['Emet', 'Jonas', 'Dev Ed', 'Brad'];
 developers.forEach(function (developer) {
     console.log(developer);
 });
 developers.filter(function (developer) {
     if (developer === 'Emet') {
-        console.log("Emet has in this array");
+        console.log("Emet has in ".concat(id, " this array"));
     }
     else {
-        console.log('No Emet There');
+        console.log('No Emet is There');
     }
 });
 console.log(id);
