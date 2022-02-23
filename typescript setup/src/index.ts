1d9b57ea5f16672c65e3a911e495e41cf2d7521c
emetdas <emet@b2bleadstaff.com>
2022-02-23 19:57:23 +0600

    ___

--- typescript setup/src/index.ts
+++ typescript setup/src/index.ts
@@ -1,5 +1,5 @@
 let id: number = 20
-let developers: string[] = ['Emet','Jonas','Dev Ed']
+let developers: string[] = ['Emet','Jonas','Dev Ed','Brad']
 
 developers.forEach((developer) =>{
     console.log(developer)
