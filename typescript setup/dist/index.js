94a6d7a8195839192bb359f342dbe0dbbb428818
emetdas <emet@b2bleadstaff.com>
2022-02-26 20:20:36 +0600

    ___

--- typescript setup/dist/index.js
+++ typescript setup/dist/index.js
@@ -1,16 +1,7 @@
 "use strict";
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-var id = [10, 5, 10, 5, 1];
-var developers = ['Emet', 'Jonas', 'Dev Ed', 'Brad'];
-developers.forEach(function (developer) {
+let id = [10, 5, 10, 5, 1];
+let developers = ['Emet', 'Jonas', 'Dev Ed', 'Brad'];
+developers.forEach((developer) => {
     console.log(developer);
 });
-var mySet = new Set(__spreadArray([], id, true));
+let mySet = new Set([...id]);
