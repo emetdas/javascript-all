a5cb1e0f2396d0e914f56c5812cdbded3a4a76f8
emetdas <emet@b2bleadstaff.com>
2022-03-02 20:31:45 +0600

    ___

--- typescript setup/src/index.ts
+++ typescript setup/src/index.ts
@@ -1,6 +1,6 @@
 let id: number[] = [10,5,10,5,1]
 let developers: string[] = ['Emet','Jonas','Dev Ed','Brad']
-developers.forEach((developer) =>{
-    console.log(developer)
+developers.forEach((developer,index) =>{
+    console.log(developer,index)
 })
 let mySet = new Set<number>([...id])
