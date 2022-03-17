abb82a9e6d56ff71eddc73d5826dba00f43f1def
emetdas <emet@b2bleadstaff.com>
2022-03-17 20:15:32 +0600

    ___update

--- typescript setup/src/index.ts
+++ typescript setup/src/index.ts
@@ -1,7 +1,6 @@
 let id: number[] = [10,5,10,5,1]
 let developers: string[] = ['Emet','Jonas','Dev Ed','Brad']
-let Name = 'emet'; 
-let developer_name: string = 'jon'
+let Name = 'emet'
 let search = developers.filter((developer) => {
    return developer.toLocaleLowerCase().includes(Name)
 })
