66ee3f531f9ed711ba1e9b18ec5faead1e183f97
emetdas <emet@b2bleadstaff.com>
2022-03-17 20:13:28 +0600

    __update

--- typescript setup/dist/index.js
+++ typescript setup/dist/index.js
@@ -1,14 +1,9 @@
 "use strict";
 let id = [10, 5, 10, 5, 1];
-// let developers: string[] = ['Emet','Jonas','Dev Ed','Brad']
-// let Name = 'emet'; 
-let developers = [
-    { title: 'Emet' },
-    { title: 'Jonas' },
-    { title: 'Dev Ed' },
-];
+let developers = ['Emet', 'Jonas', 'Dev Ed', 'Brad'];
+let Name = 'emet';
 let developer_name = 'jon';
 let search = developers.filter((developer) => {
-    return developer.title.toLocaleLowerCase().includes(developer_name);
+    return developer.toLocaleLowerCase().includes(Name);
 });
 console.log(search);
