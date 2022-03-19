2e858d3fe82768787a88a276b4d7060d2bcd5276
emetdas <emet@b2bleadstaff.com>
2022-03-19 12:04:09 +0600

    ___reduce function

--- typescript setup/dist/index.js
+++ typescript setup/dist/index.js
@@ -5,4 +5,8 @@ let Name = 'emet';
 let search = developers.filter((developer) => {
     return developer.toLocaleLowerCase().includes(Name);
 });
+let total_id = id.reduce((index, current) => {
+    return index + current;
+}, 1);
+console.log(total_id);
 console.log(search);
