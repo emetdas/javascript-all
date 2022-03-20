247038e6167559f9ee7dc803170926a695ff5bb2
emetdas <emet@b2bleadstaff.com>
2022-03-20 14:12:37 +0600

    __typescript

--- typescript setup/src/index.ts
+++ typescript setup/src/index.ts
@@ -4,8 +4,10 @@ let Name = 'emet'
 let search = developers.filter((developer) => {
    return developer.toLocaleLowerCase().includes(Name)
 })
+
 let total_id = id.reduce((index,current) => {
    return  index + current
 },1)
+
 console.log(total_id)
 console.log(search);
